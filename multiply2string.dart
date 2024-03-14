d4a78fc8f94ee20a68ead171e44a93e21e9cbee5
ajithaju634 <159872661+ajithaju634@users.noreply.github.com>
2024-03-14 16:22:34 +0530

    14/3/2024

---
+++ multiply2string.dart
@@ -0,0 +1,8 @@
+multiply() {
+  String num1 = '123';
+  String num2 = '456';
+  int number1 = int.parse(num1);
+  int number2 = int.parse(num2);
+  int result = number1 * number2;
+  print(result);
+}
